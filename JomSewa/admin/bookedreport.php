cb5c2b41b7503a2b17d44064711e959ed5460612
wsayub98 <67887529+wsayub98@users.noreply.github.com>
2020-07-29 16:56:45 +0800

    Update bookedreport.php

--- JomSewa/admin/bookedreport.php
+++ JomSewa/admin/bookedreport.php
@@ -1,5 +1,41 @@
+<?php
+    $con = mysqli_connect("localhost","root","","jomsewa");
+  if($con){
+    //echo "connected";
+  }
+ ?>
 <html>
 <head>
+
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['students', 'contribution'],
+         <?php
+         $sql = "SELECT *, COUNT(BookingID) AS booked FROM booking GROUP BY destination";
+         $fire = mysqli_query($con, $sql);
+          while ($result = mysqli_fetch_assoc($fire)) {
+            echo"['".$result['destination']."',".$result['booked']."],";
+          }
+
+         ?>
+        ]);
+
+        var options = {
+          title: 'Percentages of Destination Visited By Students.',
+          pieHole: 0.4
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+</script>
 <style>
 table,td{
 cellpadding: 10;
@@ -48,3 +84,6 @@ $i++;
 }?>
 </table>
 <?php }?>
+</body>
+ <div id="piechart" style="width: 900px; height: 500px;"></div>
+ </html>
