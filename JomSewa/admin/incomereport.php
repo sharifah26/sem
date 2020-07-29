267f3982cb73b5c2fa4cbe96546f7db45537a390
wsayub98 <67887529+wsayub98@users.noreply.github.com>
2020-07-29 16:55:50 +0800

    Update incomereport.php

--- JomSewa/admin/incomereport.php
+++ JomSewa/admin/incomereport.php
@@ -1,5 +1,40 @@
+<?php
+    $q=mysqli_query($conn,"SELECT *, SUM(payment) AS total FROM booking GROUP BY driver ");
+ ?>
 <html>
 <head>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['bar']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Driver', 'Total Income (RM)'],
+          
+          <?php
+            
+              while($row=mysqli_fetch_array($q)){
+
+                echo "['".$row['driver']."','".$row['total']."'],";
+              }
+            
+          ?>
+
+        ]);
+
+        var options = {
+          chart: {
+            title: 'Income of Drivers',
+            subtitle: 'Total Payment and Income',
+        	}
+        };
+
+        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+        chart.draw(data, google.charts.Bar.convertOptions(options));
+      }
+</script>
 <style>
 table,td{
 cellpadding: 10;
@@ -8,9 +43,10 @@ border: 1px solid black;
 </style>
 </head>
 <body>
-<?php 
-$q=mysqli_query($conn,"select * from booking ");
-$rr=mysqli_num_rows($q);
+<?php
+$query_income= "SELECT *, SUM(payment) AS total FROM booking GROUP BY driver";
+$qIncome_result = mysqli_query($conn, $query_income); 
+$rr=mysqli_num_rows($qIncome_result);
 if(!$rr)
 {
 echo "<h2 style='color:red'>No any Notice found !!!</h2>";
@@ -22,21 +58,21 @@ else
 
 <table class="table table-bordered">
 	<Tr class="success">
-	    <th>ID</th>
-		<th>Name</th>
-		<th>Price</th>
+	    <th>NO.</th>
+		<th>Name of Driver(s)</th>
+		<th>Total Income (RM)</th>
 	</Tr>
 
 <?php 
 
 $i=1;
-while($row=mysqli_fetch_assoc($q))
+while($row=mysqli_fetch_assoc($qIncome_result))
 {
 
 echo "<Tr>";
 echo "<td>".$i."</td>";
 echo "<td>".$row['driver']."</td>";
-echo "<td>".$row['payment']."</td>";
+echo "<td>".$row['total']."</td>";
 
 ?>
 <?php 
@@ -45,3 +81,6 @@ $i++;
 }?>
 </table>
 <?php }?>
+</body>
+<div id="columnchart_material" style="width: 800px; height: 500px;"></div>
+</html>
