ea672897a3057cc9979de2424925ca007db02b2f
nrain3112 <67887382+nrain3112@users.noreply.github.com>
2020-08-01 01:19:43 +0800

    Update update_notice.php

--- JomSewa/admin/update_notice.php
+++ JomSewa/admin/update_notice.php
@@ -29,11 +29,8 @@ $res=mysqli_fetch_array($q);
 		<input type="text" name="sub" value="<?php echo $res['subject']; ?>" class="form-control"/></div>
 	</div>
 	
+	<div class="row" style="margin-top:20px" class="col-sm-2" class="col-sm-8"></div>
 	
-	<div class="row" style="margin-top:10px">
-		<div class="col-sm-2"></div>
-		<div class="col-sm-8">
-	</div>	
 	
 	<div class="row">
 		<div class="col-sm-4">Enter Details</div>
@@ -42,30 +39,12 @@ $res=mysqli_fetch_array($q);
 	</div>
 	
 	
-	<div class="row" style="margin-top:10px">
+	<div class="row" style="margin-top:20px">
 		<div class="col-sm-2"></div>
 		<div class="col-sm-8">
 	</div>	
 	
-	<div class="row">
-		<div class="col-sm-4">Select User</div>
-		<div class="col-sm-5">
-		<select name="user[]" multiple="multiple" class="form-control">
-			<?php 
-	$sql=mysqli_query($conn,"select name,email from user");
-	while($r=mysqli_fetch_array($sql))
-	{
-		echo "<option value='".$r['email']."'>".$r['name']."</option>";
-	}
-			?>
-		</select>
-		</div>
-	</div>
 	
-	<div class="row" style="margin-top:10px">
-		<div class="col-sm-2"></div>
-		<div class="col-sm-8">
-	</div>	
 		
 		<div class="row" style="margin-top:10px">
 		<div class="col-sm-2"></div>
