9e2b7cfe75d37bebcd1e63868b7a312579e6eb4d
nrain3112 <67887382+nrain3112@users.noreply.github.com>
2020-08-01 01:01:42 +0800

    Update add_notice.php

--- JomSewa/admin/add_notice.php
+++ JomSewa/admin/add_notice.php
@@ -11,7 +11,7 @@ if(isset($add))
 	{
 		foreach($user as $v)
 		{
-mysqli_query($conn,"insert into notice values('','$v','$sub','$details',now())");
+			mysqli_query($conn,"insert into notice values('','$v','$sub','$details',now())");
 		}
 		
 		$err="<font color='green'>Notice added Successfully</font>";	
@@ -36,10 +36,8 @@ mysqli_query($conn,"insert into notice values('','$v','$sub','$details',now())")
 	</div>
 	
 	
-	<div class="row" style="margin-top:10px">
-		<div class="col-sm-2"></div>
-		<div class="col-sm-8">
-	</div>	
+	<div class="row" style="margin-top:20px" class="col-sm-2" class="col-sm-8"></div>
+			
 	
 	<div class="row">
 		<div class="col-sm-4">Enter Details</div>
@@ -48,30 +46,24 @@ mysqli_query($conn,"insert into notice values('','$v','$sub','$details',now())")
 	</div>
 	
 	
-	<div class="row" style="margin-top:10px">
-		<div class="col-sm-2"></div>
-		<div class="col-sm-8">
-	</div>	
+	<div class="row" style="margin-top:20px" class="col-sm-2" class="col-sm-8"></div>
 	
 	<div class="row">
 		<div class="col-sm-4">Select User</div>
 		<div class="col-sm-5">
 		<select name="user[]" multiple="multiple" class="form-control">
 			<?php 
-	$sql=mysqli_query($conn,"select name,email from user");
+	$sql=mysqli_query($conn,"select id from user");
 	while($r=mysqli_fetch_array($sql))
 	{
-		echo "<option value='".$r['email']."'>".$r['name']."</option>";
+		echo "<option value='".$r['email']."'>".$r['id']."</option>";
 	}
 			?>
 		</select>
 		</div>
 	</div>
 	
-	<div class="row" style="margin-top:10px">
-		<div class="col-sm-2"></div>
-		<div class="col-sm-8">
-	</div>	
+	<div class="row" style="margin-top:20px" class="col-sm-2" class="col-sm-8"></div>	
 		
 		<div class="row" style="margin-top:10px">
 		<div class="col-sm-2"></div>
@@ -81,3 +73,91 @@ mysqli_query($conn,"insert into notice values('','$v','$sub','$details',now())")
 		</div>
 	</div>
 </form>	
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script>
+$(document).ready(function() {
+  $(".search").keyup(function () {
+    var searchTerm = $(".search").val();
+    var listItem = $('.results tbody').children('tr');
+    var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
+    
+  $.extend($.expr[':'], {'containsi': function(elem, i, match, array){
+        return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+    }
+  });
+    
+  $(".results tbody tr").not(":containsi('" + searchSplit + "')").each(function(e){
+    $(this).attr('visible','false');
+  });
+
+  $(".results tbody tr:containsi('" + searchSplit + "')").each(function(e){
+    $(this).attr('visible','true');
+  });
+
+  var jobCount = $('.results tbody tr[visible="true"]').length;
+    $('.counter').text(jobCount + ' item');
+
+  if(jobCount == '0') {$('.no-result').show();}
+    else {$('.no-result').hide();}
+		  });
+});
+</script>
+
+<style>
+body{
+  padding:20px 20px;
+}
+
+.results tr[visible='false'],
+.no-result{
+  display:none;
+}
+
+.results tr[visible='true']{
+  display:table-row;
+}
+
+.counter{
+  padding:8px; 
+  color:#ccc;
+}
+</style>
+<div class="form-group pull-right">
+    <input type="text" class="search form-control" placeholder="Who User you looking for?">
+</div>
+<span class="counter pull-right"></span>
+<table class="table table-hover table-bordered results">
+  <thead>
+    <tr>
+      <th class="col-md-5 col-xs-5">Id</th>
+      <th class="col-md-4 col-xs-4">Name</th>
+      <th class="col-md-3 col-xs-3">Gender</th>
+	  <th class="col-md-3 col-xs-3">Services</th>
+    </tr>
+    <tr class="warning no-result">
+      <td colspan="4"><i class="fa fa-warning"></i> No result</td>
+    </tr>
+  </thead>
+  <tbody>
+     <tr>
+	  	 <?php 
+
+			$sql=mysqli_query($conn,"select * from user");
+			$i=1;
+			while($row=mysqli_fetch_array($sql)){
+		  ?>
+		<th scope="row"> <?php echo $row['id'];?></th>
+		<td><?php echo $row['name'];?></td>
+		<td><?php echo $row['gender'];?></td>
+		<td><?php echo $row['services'];?></td>
+	</tr>
+<?php 
+echo "</Tr>";
+$i++;
+}?>
+	 
+  </tbody>
+</table>
