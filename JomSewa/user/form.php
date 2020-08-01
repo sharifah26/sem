eaff58d261fe992ae5910c77e8529cb8af74924a
ArifRazi <67832652+ArifRazi@users.noreply.github.com>
2020-08-01 21:27:57 +0800

    update form.php
    add online banking checkbox, print receipt

--- JomSewa/user/form.php
+++ JomSewa/user/form.php
@@ -145,6 +145,7 @@ $users=mysqli_fetch_assoc($sql);
 		?>
 <fieldset>
 		<center>
+			<div id="receipt"> 
 			Destination :
 			<?php
 				echo $_POST["destination"]; 
@@ -165,12 +166,33 @@ $users=mysqli_fetch_assoc($sql);
 			<?php
 				echo $row2['distance'];
 			?><br><br>
+			</div>
+			<table border=0 cellpadding="10">
+          		<tr>
+           		 <th><input type="checkbox"  value="bank islam" class="check"><img src="payment images/bank islam.png" width="90px" height="60px"></th>
+            		 <th><input type="checkbox"  value="cimb" class="check"><img src="payment images/cimb.png" width="90px" height="60px"></th>
+                	 <th><input type="checkbox"  value="may bank" class="check"><img src="payment images/may bank.png" width="90px" height="60px"></th>
+                	 <th><input type="checkbox"  value="rnb" class="check"><img src="payment images/rhb.png" width="90px" height="60px"></th>
+           		 </tr>
+           		 </table>
+           		 <br><br>
 			<button onclick="myFunction()">Pay</button>
+			<button onclick="printContent('receipt')">Print</button>
 </fieldset>
 <script>
 function myFunction() {
   alert("Payment Successful. Estimated arrival : 15 mins");
 }
+</script>
+	
+<script>
+	function printContent(el){
+		var restorepage = document.body.innerHTML;
+		var printContent = document.getElementById(el).innerHTML;
+		document.body.innerHTML = printContent;
+		window.print();
+		document.body.innerHTML = restorepage;
+	}
 </script>
 		</center>
 				   
